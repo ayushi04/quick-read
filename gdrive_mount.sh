1b65a924eb767103cc85319b2dd5c2c6b19703cd
AYUSHI GUPTA <ayushi.gupta@research.iiit.ac.in>
2019-10-08 19:26:06 +0530

    Create gdrive_mount.sh
    Run these commands to mount google drive on  ubuntu 14.04

---
+++ gdrive_mount.sh
@@ -0,0 +1,7 @@
+#refer link : https://www.linuxtechi.com/mount-google-drive-in-ubuntu/
+
+sudo add-apt-repository ppa:thefanclub/grive-tools
+sudo apt-get update
+sudo apt-get install grive-tools
+
+#then search for grive and signin to your account
